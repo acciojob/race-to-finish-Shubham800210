4d60184ca03752ee9ea3712d965bc206f2bda1e0
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-17 21:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 window.promises = [];
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 5 + 1), Math.random() * 5000))
+];
 
+Promise.any(promises)
+  .then(result => {
+    document.getElementById("output").innerText = `The first resolved promise had a value of ${result}.`;
+  })
+  .catch(error => {
+    console.error(error);
+  });
 // Do not change the code above this
 // add your promises to the array `promises`
